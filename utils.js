69d453666c79b23218e9a6f408b6db0704ab934a
Terence Sam <sam.p.terence@gmail.com>
2022-07-01 12:11:51 -0600

    fixed .js

--- utils.js
+++ utils.js
@@ -1,7 +1,5 @@
 function shuffleArray(array) {
-
-    let arrCopy = [1, "test", true, 2]
-    
+    let arrCopy = [...array]
     for (let i = arrCopy.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [arrCopy[i], arrCopy[j]] = [arrCopy[j], arrCopy[i]];
